92836fbd45717a6e32cc3f5ad606f9827eeaa6ad
Max Krieg <maxwell.krieg@gmail.com>
2021-02-03 08:08:51 -0700

    fix error message

--- lib/utils.py
+++ lib/utils.py
@@ -3,7 +3,11 @@ import sys
 
 def exit_if_no_data(data, name):
     if data is None or len(data) < 1:
-        print("Oops, we were unable to gather {name} data, exiting")
+        print(
+            "Sorry, we were unable to gather {name} data from the MBTA API, exiting".format(
+                name=name
+            )
+        )
         sys.exit()
 
 
